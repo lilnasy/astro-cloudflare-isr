0cef615300a1e4680c19df17c240ccb8a6de4349
Arsh <69170106+lilnasy@users.noreply.github.com>
2023-06-02 17:10:52 +0530

    Add files

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+
+async function cachingMiddleware({ request }: { request: Request }, next : () => Promise<Response>) {
+    const cache = (caches as any).default as Cache
+    
+    const match = await cache.match(request)
+
+    if (match !== undefined) return match
+
+    const response = await next()
+    
+    if (response.ok && response.headers.has('Cache-Control'))
+        cache.put(request, response.clone()).catch(_ => _)
+}
+
+export const onRequest =
+    typeof globalThis.CacheStorage === 'function' && globalThis.caches instanceof globalThis.CacheStorage
+        ? cachingMiddleware
+        : (_, next) => { next() }
